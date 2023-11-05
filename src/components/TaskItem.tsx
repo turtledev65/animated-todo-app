d7965b9915927452f0a644cebd1a57e3f8d3f86c
turtledev <turtledev65@gmail.com>
2023-11-05 20:13:30 +0200

    Make the task item component deletable by swiping it to the left

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,37 +1,47 @@
-import { Checkbox, HStack } from "@chakra-ui/react";
+import { Checkbox, HStack, Icon } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { FiTrash2 } from "react-icons/fi";
 import Task from "../entities/Task";
 import AnimatedTaskLabel from "./AnimatedTaskLabel";
+import SwipeableBox from "./SwipeableBox";
 
 interface TaskItemProps {
   task: Task;
-  onToggleDone: () => void;
+  onToggle: () => void;
+  onDelete: () => void;
 }
 
 const AnimatedHStack = motion(HStack);
 
-const TaskItem = ({ task, onToggleDone }: TaskItemProps) => {
+const TaskItem = ({ task, onToggle, onDelete }: TaskItemProps) => {
   return (
-    <AnimatedHStack
-      width="100%"
-      gap={4}
-      py={1}
-      px={2}
-      initial={{ scale: 0 }}
-      animate={{
-        scale: "100%",
-        transition: {
-          type: "spring",
-        },
-      }}
+    <SwipeableBox
+      onSwipe={onDelete}
+      backElement={
+        <AnimatedHStack
+          w="full"
+          h="full"
+          bg="red.500"
+          pr={3}
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+          <Icon as={FiTrash2} boxSize={6} />
+        </AnimatedHStack>
+      }
     >
-      <Checkbox
-        size="lg"
-        isChecked={task.done}
-        onChange={() => onToggleDone()}
-      />
-      <AnimatedTaskLabel label={task.label} done={task.done} />
-    </AnimatedHStack>
+      <AnimatedHStack
+        w="full"
+        h="full"
+        gap={4}
+        py={2}
+        px={2}
+        backgroundColor={"gray.800"}
+      >
+        <Checkbox size="lg" isChecked={task.done} onChange={() => onToggle()} />
+        <AnimatedTaskLabel label={task.label} done={task.done} />
+      </AnimatedHStack>
+    </SwipeableBox>
   );
 };
 

1173c95cd5a4f4d35995f96dbb66521074917a95
turtledev <turtledev65@gmail.com>
2023-11-10 23:19:01 +0200

    Add custom theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,44 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const colors = {
+  bg: {
+    _light: "gray.100",
+    _dark: "gray.900",
+  },
+  fg: {
+    _light: "#0a0a0a",
+    _dark: "gray.200",
+  },
+  fgSecondary: {
+    _light: "gray.400",
+    _dark: "gray.500",
+  },
+  fgCrossed: {
+    _light: "gray.400",
+    _dark: "gray.600",
+  },
+  border: {
+    _light: "gray.400",
+    _dark: "gray.700",
+  },
+  blue: {
+    _light: "blue.300",
+    _dark: "blue.500",
+  },
+  yellow: {
+    _light: "yellow.300",
+    _dark: "yellow.500",
+  },
+  purple: {
+    _light: "purple.300",
+    _dark: "purple.600",
+  },
+  red: {
+    _light: "red.400",
+    _dark: "red.500",
+  },
+};
+
 const theme = extendTheme({
   // necessary for animating theme changes
   styles: {
@@ -14,6 +53,34 @@ const theme = extendTheme({
   config: {
     initialColorMode: "dark",
     disableTransitionOnChange: false, // don't disable transitions when changing the theme
+  },
+  semanticTokens: {
+    colors: {
+      ...colors,
+      "chakra-body-bg": { ...colors.bg },
+      "icon-color": { ...colors.fg },
+      "text-normal": { ...colors.fg },
+      "text-secondary": { ...colors.fgSecondary },
+      "text-crossed": { ...colors.fgCrossed },
+    },
+  },
+  components: {
+    Checkbox: {
+      parts: ["control"],
+      baseStyle: {
+        control: {
+          borderRadius: "5px",
+          _checked: {
+            _hover: {
+              bg: "blue",
+              borderColor: "blue",
+            },
+            bg: "blue",
+            borderColor: "blue",
+          },
+        },
+      },
+    },
   },
 });
 

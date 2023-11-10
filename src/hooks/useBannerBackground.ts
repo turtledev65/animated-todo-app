9eeee4dab54776bb633ba1d02f64c1c0dcf504a9
turtledev <turtledev65@gmail.com>
2023-11-10 22:41:50 +0200

    Save and load the banner to session storage, instead of changing it every time the page is loaded

--- src/hooks/useBannerBackground.ts
+++ src/hooks/useBannerBackground.ts
@@ -4,11 +4,22 @@ import banner3 from "../assets/list-banners/Saly-36.png";
 import banner4 from "../assets/list-banners/Saly-43.png";
 import pickRandomItem from "../utils/pickRandomItem";
 
-const images = [banner1, banner2, banner3, banner4];
-const colors = ["yellow.500", "blue.500", "orange.500", "purple.500"];
+const bannerImages = [banner1, banner2, banner3, banner4];
+const bannerColors = ["yellow.500", "blue.500", "orange.500", "purple.500"];
 
 const useBannerBackground = () => {
-  return { image: pickRandomItem(images), color: pickRandomItem(colors) };
+  const storedBannerString = sessionStorage.getItem("banner");
+  if (storedBannerString) {
+    const storedBanner = JSON.parse(storedBannerString);
+    return storedBanner;
+  }
+
+  const banner = {
+    image: pickRandomItem(bannerImages),
+    color: pickRandomItem(bannerColors),
+  };
+  sessionStorage.setItem("banner", JSON.stringify(banner));
+  return banner;
 };
 
 export default useBannerBackground;

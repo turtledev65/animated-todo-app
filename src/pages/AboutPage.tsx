9751dfb11a82bbf89433677692a6ee3f790fd3a5
turtledev <turtledev65@gmail.com>
2023-11-12 09:49:09 +0200

    Make the color of the pwa theme dynamic

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,58 +1,64 @@
 import { Box, Button, Heading, Text, VStack } from "@chakra-ui/react";
 import { BsGithub } from "react-icons/bs";
 import bannerImage from "../assets/about-banner.png";
+import useChakraHexColor from "../hooks/useChakraHexColor";
 
 const AboutPage = () => {
+  const bgHexColor = useChakraHexColor("blue", "#3182CE");
+
   return (
-    <VStack h="100vh" pos="relative" gap={0} bg="blue">
-      <Box
-        backgroundSize="cover"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        backgroundImage={bannerImage}
-        flex={1.5}
-        w="full"
-        h="full"
-        display="flex"
-        flexDir="column"
-        justifyContent="flex-end"
-        px="15px"
-        py="10px"
-      >
-        <Heading fontSize="4xl" color="white">
-          About this app
-        </Heading>
-      </Box>
-      <Box
-        w="full"
-        h="full"
-        bg="bg"
-        borderTopRadius="15px"
-        overflowX="hidden"
-        position="relative"
-        px="10px"
-        py="20px"
-        flex={3}
-        display="flex"
-        flexDir="column"
-        justifyContent="space-between"
-      >
-        <Text fontSize="xl">
-          This is a React web app meant to be used as a PWA. It uses Chakra UI
-          and Framer Motion for animations.
-        </Text>
-        <Button
-          as="a"
-          href="https://github.com/turtledev65/animated-todo-app"
-          target="_blank"
-          leftIcon={<BsGithub />}
+    <>
+      <meta name="theme-color" content={bgHexColor} />
+      <VStack h="100vh" pos="relative" gap={0} bg="blue">
+        <Box
+          backgroundSize="cover"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          backgroundImage={bannerImage}
+          flex={1.5}
           w="full"
-          mt="10px"
+          h="full"
+          display="flex"
+          flexDir="column"
+          justifyContent="flex-end"
+          px="15px"
+          py="10px"
         >
-          Source Code
-        </Button>
-      </Box>
-    </VStack>
+          <Heading fontSize="4xl" color="white">
+            About this app
+          </Heading>
+        </Box>
+        <Box
+          w="full"
+          h="full"
+          bg="bg"
+          borderTopRadius="15px"
+          overflowX="hidden"
+          position="relative"
+          px="10px"
+          py="20px"
+          flex={3}
+          display="flex"
+          flexDir="column"
+          justifyContent="space-between"
+        >
+          <Text fontSize="xl">
+            This is a React web app meant to be used as a PWA. It uses Chakra UI
+            and Framer Motion for animations.
+          </Text>
+          <Button
+            as="a"
+            href="https://github.com/turtledev65/animated-todo-app"
+            target="_blank"
+            leftIcon={<BsGithub />}
+            w="full"
+            mt="10px"
+          >
+            Source Code
+          </Button>
+        </Box>
+      </VStack>
+    </>
   );
 };
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,39 +4,44 @@ import AddTaskButton from "../components/AddTaskButton";
 import TaskList from "../components/TaskList";
 import TaskItemsProvider from "../context/TaskItemsProvider";
 import useBannerBackground from "../hooks/useBannerBackground";
+import useChakraHexColor from "../hooks/useChakraHexColor";
 
 const AnimatedBox = motion(Box);
 
 const MainPage = () => {
   const { image: bannerImage, color: bannerBgColor } = useBannerBackground();
+  const bgHexColor = useChakraHexColor(bannerBgColor, "#3182CE");
 
   return (
-    <TaskItemsProvider>
-      <VStack h="100vh" pos="relative" gap={0} bg={bannerBgColor}>
-        <Box
-          backgroundSize="cover"
-          backgroundPosition="top"
-          backgroundRepeat="no-repeat"
-          backgroundImage={bannerImage}
-          flex={1.5}
-          w="full"
-          h="full"
-        />
-        <AnimatedBox
-          w="full"
-          h="full"
-          bg="bg"
-          pt="20px"
-          borderTopRadius="15px"
-          overflowX="hidden"
-          position="relative"
-          flex={3}
-        >
-          <TaskList />
-          <AddTaskButton pos="fixed" bottom="25px" right="25px" />
-        </AnimatedBox>
-      </VStack>
-    </TaskItemsProvider>
+    <>
+      <meta name="theme-color" content={bgHexColor} />
+      <TaskItemsProvider>
+        <VStack h="100vh" pos="relative" gap={0} bg={bannerBgColor}>
+          <Box
+            backgroundSize="cover"
+            backgroundPosition="top"
+            backgroundRepeat="no-repeat"
+            backgroundImage={bannerImage}
+            flex={1.5}
+            w="full"
+            h="full"
+          />
+          <AnimatedBox
+            w="full"
+            h="full"
+            bg="bg"
+            pt="20px"
+            borderTopRadius="15px"
+            overflowX="hidden"
+            position="relative"
+            flex={3}
+          >
+            <TaskList />
+            <AddTaskButton pos="fixed" bottom="25px" right="25px" />
+          </AnimatedBox>
+        </VStack>
+      </TaskItemsProvider>
+    </>
   );
 };
 

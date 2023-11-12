38d94e747059ddceaec346d2ae9a279c2d55fbef
turtledev <turtledev65@gmail.com>
2023-11-12 10:14:04 +0200

    Fix wrong homepage

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/animated-todo-app/sw.js', { scope: '/animated-todo-app/' })})}

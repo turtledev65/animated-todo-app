7e343217aeb3c92c349792d4ee3652a099943829
turtledev <turtledev65@gmail.com>
2023-11-27 20:29:49 +0200

    Update example firebse config

--- firebase.config.example.ts
+++ firebase.config.example.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "",
@@ -11,4 +12,8 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const provider = new GoogleAuthProvider();
